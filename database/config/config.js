89785a2aa69242a481c7b0e93d1374892339e6c7
Luan Vasco <luan.vfarias@gmail.com>
2020-11-12 18:41:02 -0300

    add db name in prod env

--- database/config/config.js
+++ database/config/config.js
@@ -15,7 +15,7 @@ module.exports = {
     username: process.env.DB_USER,
     password: process.env.DB_PW,
     host: process.env.DB_HOST,
-    database: 'whims_music',
+    database: process.env.DB_NAME,
     dialect: 'postgres',
   },
 };
